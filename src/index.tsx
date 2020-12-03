58e1779dbeb1f0f900e406c255336fb747b97e50
sergeyorolov <korolovsergio@mail.ru>
2020-12-04 02:24:37 +0300

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 
 const app = (
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
